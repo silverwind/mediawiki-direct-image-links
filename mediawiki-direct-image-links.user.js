f649a0138b3bb3749759c78407b974731e54e0e4
silverwind <silv3rwind@gmail.com>
2013-06-10 15:06:40 +0200

    Update script to work on all mediawiki sites

---
+++ mediawiki-direct-image-links.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name          mediawiki-direct-image-links
+// @description   Replaces links to images on mediawiki sites with direct links.
+// @version       0.2
+// @license       http://sam.zoy.org/wtfpl/COPYING
+// @namespace     https://github.com/silverwind
+// @updateURL     https://github.com/silverwind/mediawiki-direct-image-links/raw/master/mediawiki-direct-image-links.user.js
+// @downloadURL   https://github.com/silverwind/mediawiki-direct-image-links/raw/master/mediawiki-direct-image-links.user.js
+// @include       http*
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js
+// ==/UserScript==
+
+"use strict";
+
+$("a.image[href*='File:']").each(function () {
+    var currentImage = $(this);
+    $.get($(this).attr("href"), function (data) {
+        var imgurl = $(".fullImageLink", data).find("a").attr("href");
+        $(currentImage).attr("href", imgurl);
+    });
+});
